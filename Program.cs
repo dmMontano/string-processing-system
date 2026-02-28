7a09e04c90910d6f1c6765a35fdfbc13c1ba6439
Dannicah <dannicahmontano@gmail.com>
2026-02-28 11:26:15 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using StringProcessingApp.Views;
+
+namespace StringProcessingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringView view = new StringView();
+            view.Run();
+        }
+    }
+}
